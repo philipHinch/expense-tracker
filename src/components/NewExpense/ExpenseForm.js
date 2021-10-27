28d6eb6d8dc32b79ea5de1392c752ba7296dcece
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-10-27 19:54:12 +0300

    added filter funcionality

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 
     const [enteredTitle, setEnteredTitle] = useState('')
     const [enteredAmount, setEnteredAmount] = useState('')
@@ -28,11 +28,12 @@ const ExpenseForm = () => {
             date: new Date(enteredDate)
         }
 
+        props.onSaveExpenseData(expenseData)
+
         setEnteredTitle('')
         setEnteredAmount('')
         setEnteredDate('')
 
-        console.log(expenseData);
     }
 
     return (
